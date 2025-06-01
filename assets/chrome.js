dc7940fd1d5db547dfc42446176c4d9e7cf7d1ac
zeeshan <45302947+zeeshanarshad44@users.noreply.github.com>
2025-06-02 04:46:06 +0500

    (edit) added content to js files

--- assets/chrome.js
+++ assets/chrome.js
@@ -1,1 +1,72 @@
-chrome
+// Inspiration via: http://davidwalsh.name/css-animation-callback
+var addAnimationEndEvent = function(el, fn) {
+  var t;
+  var animations = {
+    'animation'       : 'animationend',
+    'OAnimation'      : 'oAnimationEnd',
+    'MozAnimation'    : 'animationend',
+    'WebkitAnimation' : 'webkitAnimationEnd'
+  };
+
+  for (t in animations) {
+    if ( typeof el.style[t] !== 'undefined' ) {
+      el.addEventListener(animations[t], fn);
+      return true;
+    }
+  }
+  return false;
+};
+
+var removeAnimationEndEvent = function(el, fn) {
+  var t;
+  var animations = {
+    'animation'       : 'animationend',
+    'OAnimation'      : 'oAnimationEnd',
+    'MozAnimation'    : 'animationend',
+    'WebkitAnimation' : 'webkitAnimationEnd'
+  };
+
+  for (t in animations) {
+    if ( typeof el.style[t] !== 'undefined' ) {
+      el.removeEventListener(animations[t], fn);
+      return true;
+    }
+  }
+  return false;
+};
+
+var addTransitionEndEvent = function(el, fn) {
+  var t;
+  var transitions = {
+    'transition'       : 'transitionend',
+    'OTransition'      : 'oTransitionEnd',
+    'MozTransition'    : 'transitionend',
+    'WebkitTransition' : 'webkitTransitionEnd'
+  };
+
+  for (t in transitions) {
+    if ( typeof el.style[t] !== 'undefined' ) {
+      el.addEventListener(transitions[t], fn);
+      return true;
+    }
+  }
+  return false;
+};
+
+var removeTransitionEndEvent = function(el, fn) {
+  var t;
+  var transitions = {
+    'transition'       : 'transitionend',
+    'OTransition'      : 'oTransitionEnd',
+    'MozTransition'    : 'transitionend',
+    'WebkitTransition' : 'webkitTransitionEnd'
+  };
+
+  for (t in transitions) {
+    if ( typeof el.style[t] !== 'undefined' ) {
+      el.removeEventListener(transitions[t], fn);
+      return true;
+    }
+  }
+  return false;
+};
--- assets/tool.js
+++ assets/tool.js
@@ -1,1 +1,23 @@
-tool
+<script type="text/javascript">
+/**
+ * Attach an event handler on a given Node taking care of Browsers Differences
+ * @param {Object} node
+ * @param {String} type
+ * @param {Function} fn
+ * @param {Boolean} capture
+ */
+function addEventHandler(node,type,fn , capture){
+       if(typeof window.event !== "undefined"){
+                /* Internet Explorer way */
+               node.attachEvent( "on" + type, fn );
+       } else {
+               /* FF & Other Browsers */
+               node.addEventListener( type, fn , capture );
+       }
+}
+
+/* Example */
+addEventHandler(window,"load",function(){
+   alert("The page was loaded");
+},true)
+</script>
